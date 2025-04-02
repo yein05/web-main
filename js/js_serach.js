c08987122480b556cc7f7ca137f330575adcb919
admin <yein6451@gmaill.com>
2025-04-02 11:28:08 +0900

    5주차 깃허브 테스트트

---
+++ js/js_serach.js
@@ -0,0 +1,13 @@
+document.getElementById("search_btn").addEventListener('click', search_message);
+
+function search_message(){
+alert("검색을 수행합니다!");
+}
+
+function googleSearch() {
+    const searchTerm = document.getElementById("search_input").value; // 검색어로 설정
+    const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
+    // 새 창에서 구글 검색을 수행
+    window.open(googleSearchUrl, "_blank"); // 새로운 창에서 열기.
+    return false;
+    }
