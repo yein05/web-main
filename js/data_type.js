9ec3dfb6f8bccceab00bfee3bc79f09eefa50ed4
admin <yein6451@gmaill.com>
2025-04-09 10:57:20 +0900

    6주차 깃허브

---
+++ js/data_type.js
@@ -0,0 +1,30 @@
+let number = 5;
+let str = 문자열입력; // “ “도 묶음 가능
+let prime = 1.5123;
+let is_ok = true; // 참
+let is_not = false; // 거짓
+let undefi; // 변수 이름만, 초기화 x
+let empty = null; // 비어 있음
+
+console.log(undefi, empty); // 여러 개 출력
+
+const users = new Map(); // 사용자 정보 Map 객체 생성
+users.set("user1", { // 사용자 정보 추가
+id: 1, password: "password123",
+});
+users.set("user2", {
+id: 2, password: "password456",
+});
+// Map 객체의 모든 사용자 정보 반복 출력
+for (const [username, user] of users) {
+console.log(`사용자 이름: ${username}`, `ID: ${user.id}`);
+console.log(`비밀번호: ${user.password}`);
+}
+// Set 객체 활용 (예), 이름만 저장할 Set 객체 생성
+const usernames = new Set();
+usernames.add("user1"); // 사용자 이름 추가
+usernames.add("user2");
+// Set 객체의 모든 사용자 이름 반복 출력
+for (const username of usernames) {
+console.log(`사용자 이름: ${username}`);
+}
--- js/js_serach.js
+++ js/js_serach.js
@@ -4,6 +4,7 @@ function search_message(){
 alert("검색을 수행합니다!");
 }
 
+
 function googleSearch() {
     const searchTerm = document.getElementById("search_input").value; // 검색어로 설정
     const googleSearchUrl = `https://www.google.com/search?q=${encodeURIComponent(searchTerm)}`;
@@ -11,3 +12,5 @@ function googleSearch() {
     window.open(googleSearchUrl, "_blank"); // 새로운 창에서 열기.
     return false;
     }
+
+  
