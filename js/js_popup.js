1ef79ff418ea0f080a6d8303d68c21bce1514362
admin <yein6451@gmaill.com>
2025-04-16 10:45:19 +0900

    7주차 깃허브브

--- js/js_popup.js
+++ js/js_popup.js
@@ -2,6 +2,43 @@ function pop_up() {
     window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
     }
 
+
+    /*var close_time; // 시간 정보
+    var close_time2 = 10; // 10초 설정
+    clearTimeout(close_time); // 재호출 정지
+    close_time= setTimeout("close_window()", 10000);
+    // 1/1000 초 지정, 바로 시작
+    show_time(); // 실시간 시간 보여주기
+
+
+    function show_time(){
+    let divClock = document.getElementById('Time');
+    divClock.innerText = close_time2; // 10초 삽입 시작
+    close_time2--; // 1초씩 감소
+    setTimeout(show_time, 1000); //1초마다 갱신
+    }
+    function close_window() { // 함수 정의
+window.close(); // 윈도우 닫기
+    }*/
+
+
+    function over(obj) {
+        obj.src="image/logn.png";
+        }
+
+
+    function out(obj) {
+        obj.src="image/logo2.png";
+        }
+        //const over = (obj) => {
+         //    obj.src = "image/LOGO.png";
+        //};
+
+        //const search_message = () => {
+        //    const c = '검색을 수행합니다';
+       //     alert(c);
+       // };
+
     function show_clock(){
         let currentDate = new Date(); // 현재 시스템 날짜 객체 생성
         let divClock = document.getElementById('divClock');
@@ -23,9 +60,4 @@ function pop_up() {
         setTimeout(show_clock, 1000); //1초마다 갱신
         }
 
-        function over(obj) {
-            obj.src="image/logn.png";
-            }
-            function out(obj) {
-            obj.src="image/logo2.jpg";
-            }
+        
