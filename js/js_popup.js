66fe3ac63b2f3f5bd1297bb2a8be6d783c13abab
admin <yein6451@gmaill.com>
2025-04-30 10:49:38 +0900

    9주차 깃허브 테스트(2)

--- js/js_popup.js
+++ js/js_popup.js
@@ -1,6 +1,45 @@
     function pop_up() {
         window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
         }
+    function close_window(){
+    window.close();
+    }
+
+     function setCookie(name, value, expiredays) {
+        var date = new Date();
+        date.setDate(date.getDate() + expiredays);
+        document.cookie = escape(name) + "=" + escape(value) + ";expires=" + date.toUTCString() + "; path=/";
+        }
+
+        function getCookie(name) {
+            var cookie = document.cookie;
+            console.log("쿠키를 요청합니다.");
+            if (cookie != "") {
+            var cookie_array = cookie.split("; ");
+            for ( var index in cookie_array) {
+            var cookie_name = cookie_array[index].split("=");
+            if (cookie_name[0] == "popupYN") {
+            return cookie_name[1];
+            }
+            }
+            }
+            return ;
+            }
+
+            function closePopup() {
+                if (document.getElementById('check_popup').value) {
+                setCookie("popupYN", "N", 1);
+                console.log("쿠키를 설정합니다.");
+                self.close();
+                }
+                }
+
+        function pop_up() {
+            var cookieCheck = getCookie("popupYN");
+            if (cookieCheck != "N"){
+            window.open("../popup/popup.html", "팝업테스트", "width=400, height=300, top=10, left=10");
+            }
+            }
 
     function over(obj) {
         obj.src="image/logn.png";
